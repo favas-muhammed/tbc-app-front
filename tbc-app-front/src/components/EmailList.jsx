a1e0c8bb17d96005084d5ebd97ab380637a37e4b
Favas Muhammed <favassakkeer13@gmail.com>
2025-04-29 14:05:01 +0200

    ..

--- tbc-app-front/src/components/EmailList.jsx
+++ tbc-app-front/src/components/EmailList.jsx
@@ -74,6 +74,7 @@ const EmailList = () => {
         style={{
           display: "grid",
           gridTemplateColumns: "repeat(auto-fill, minmax(250px, 1fr))",
+          padding: "100px",
           gap: "10px",
         }}
       >
